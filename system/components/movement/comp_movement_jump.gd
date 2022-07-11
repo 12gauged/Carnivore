a98b4fa26e8d79f65f00329a60d670aeb09c028d
FelipeBdC <batatanotube@gmail.com>
2022-07-11 11:50:02 -0300

    fixed frog jump

--- system/components/movement/comp_movement_jump.gd
+++ system/components/movement/comp_movement_jump.gd
@@ -15,6 +15,10 @@ var direction: Vector2
 var force: float = 0
 
 
+var jump_initial_pos: Vector2
+var jump_final_pos: Vector2
+
+
 func _ready():
 	Target = toolbox.get_node_in_group(target_node_group)
 
@@ -35,32 +39,35 @@ func _execute(delta):
 		return
 		
 	jump_position.y += GRAVITY * delta
-	force -= 1500 * delta
+	force -= 1000 * delta
 	
 	if force <= 0.0:
+		jump_final_pos = Owner.global_position
 		emit_signal("component_value_update", Vector2.ZERO)
 		emit_signal("jump_finished")
 		Owner.enable_collision()
 		return
-	
 	emit_signal("component_value_update", Owner.global_position.direction_to(jump_position))
 	
 	
 	
 func start_jump():
+	if !is_instance_valid(Target): return
+	
 	Owner.disable_collision()
 	
 	target_position = Target.global_position
 	
 	var distance_to_target = Owner.global_position.distance_to(target_position)
-	
 	var force_multiplier: float = 5.0
-	force = min(100, distance_to_target * 0.5) if distance_to_target > 100 else distance_to_target
+	
+	force = min(80, distance_to_target * 2)
+	var original_force = force
 	force *= force_multiplier
 	
 	jump_position = Vector2(
 		Owner.global_position.x + Owner.global_position.direction_to(target_position).x * jump_force,
-		Owner.global_position.y + Owner.global_position.direction_to(target_position).y * jump_force - force / force_multiplier - 10
+		Owner.global_position.y + Owner.global_position.direction_to(target_position).y * jump_force - original_force - 10
 	)
 	
 func set_target(value: Vector2):
