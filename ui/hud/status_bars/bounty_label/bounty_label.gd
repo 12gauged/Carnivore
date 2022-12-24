257baceb8b7745836bbdd54f9a45474e0981bc35
FelipeBdC <batatanotube@gmail.com>
2022-12-24 10:22:15 -0300

    enemy changes and polish
    .increased the bounty values for each enemy
    .made the bounty label not show when playing the tutorial

--- ui/hud/status_bars/bounty_label/bounty_label.gd
+++ ui/hud/status_bars/bounty_label/bounty_label.gd
@@ -3,11 +3,19 @@ extends Label
 
 onready var AnimPlayer: AnimationPlayer = $AnimationPlayer
 var player_bounty: int
+var post_tutorial_bounty: int
 
 
 func _ready():
 	set_player_bounty(game_data.get_player_data("bounty"))
 	player_events.connect("bounty_increased", self, "_on_bounty_increased")
+	
+	post_tutorial_bounty = game_data.DEFAULT_BOUNTY + 10
+	
+	if player_bounty > post_tutorial_bounty:
+		AnimPlayer.play("highlight_long")
+		return
+	self.modulate.a = 0
 	
 	
 func set_player_bounty(value):
@@ -17,5 +25,6 @@ func set_player_bounty(value):
 
 func _on_bounty_increased(value: int):
 	set_player_bounty(player_bounty + value)
+	if player_bounty < post_tutorial_bounty: return
 	AnimPlayer.play("highlight")
 	
