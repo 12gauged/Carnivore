0cd89ded65a5e65aaf5ad886f81060c193d48c08
FelipeBdC <batatanotube@gmail.com>
2022-12-24 14:11:26 -0300

    saving flag balancing

--- scenes/levels/level1/saving_flag_spawner.gd
+++ scenes/levels/level1/saving_flag_spawner.gd
@@ -5,16 +5,16 @@ const BOUNTY_OFFSET = 100
 onready var FlagSpawner: Node2D = $flag_spawner
 var can_spawn: bool = true
 var target_bounty: int = 0
+var target_bounty_multiplier
 
 func _ready():
-	target_bounty = game_data.get_player_data("bounty") + BOUNTY_OFFSET
+	var player_bounty: float = game_data.get_player_data("bounty")
+	target_bounty_multiplier = int(ceil(player_bounty / 100.0 * int(player_bounty > 100)) + 1)
+	target_bounty = game_data.get_player_data("bounty") + BOUNTY_OFFSET * target_bounty_multiplier
 
 
 func _process(delta):
-	if !can_spawn: return
-	if game_data.level_player_bounty >= target_bounty:
-		FlagSpawner.spawn_entity()
-		target_bounty += BOUNTY_OFFSET
+	pass
 
 
 func _on_flag_spawner_entity_spawned(InstancedEntity):
@@ -23,3 +23,11 @@ func _on_flag_spawner_entity_spawned(InstancedEntity):
 	
 func _on_flag_deleted():
 	can_spawn = true
+
+
+func _on_part_arena_wave_started():
+	if !can_spawn: return
+	if game_data.level_player_bounty >= target_bounty:
+		FlagSpawner.spawn_entity()
+		target_bounty += BOUNTY_OFFSET
+		target_bounty *= target_bounty_multiplier
