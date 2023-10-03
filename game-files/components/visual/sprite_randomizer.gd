ae9958d226152cff965b636e4a7e440b2025e2a1
FelipeBdC <batatanotube@gmail.com>
2023-10-03 14:49:18 -0300

    added tree sprites to the first level

--- game-files/components/visual/sprite_randomizer.gd
+++ game-files/components/visual/sprite_randomizer.gd
@@ -1,25 +1,11 @@
-@tool
 extends Sprite2D
 class_name RandomSprite
 
 
-@export var SpriteList: Array[Texture] = []:
-	set = set_sprite_list
-@export var chosen_sprite: Texture
+@export var SpriteList: Array[Texture] = []
 	
 	
 func _ready() -> void:
-	if not Engine.is_editor_hint(): 
-		self.texture = chosen_sprite
-		return
-	set_random_texture()
-
-
-func set_sprite_list(value: Array[Texture]) -> void:
-	if not Engine.is_editor_hint(): return
-	
-	SpriteList = value
-	if SpriteList.is_empty(): return
 	set_random_texture()
 
 
@@ -27,5 +13,4 @@ func set_random_texture() -> void:
 	randomize()
 	var sprite_id = randi_range(0, len(SpriteList) - 1)
 	self.texture = SpriteList[sprite_id]
-	chosen_sprite = self.texture
 
