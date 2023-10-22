17bc1bfcd193b18b9da2d8fb9d264afb0303a2a0
FelipeBdC <batatanotube@gmail.com>
2023-10-22 14:32:46 -0300

    Added fade-in and out to in-game uis

--- game-files/ui/layouts/ingame/ingame_ui.gd
+++ game-files/ui/layouts/ingame/ingame_ui.gd
@@ -20,19 +20,62 @@ func _ready() -> void:
 	
 func open_ingame_ui(args: Array) -> void:
 	var ui_name: String = args[0]
+	var fade_in: bool = false
+	var fade_duration: float = 1.0
 	open_ui_key = ui_name
+	
+	match len(args):
+		2:
+			fade_in = args[1]
+		4:
+			fade_in = args[1]
+			fade_duration = args[3]
 	
 	if not ui_name in menus.keys():
 		push_error("%s not present in menus.keys()" % ui_name)
 		return
-	color_rect.show()
-	menus[ui_name].visible = true
+	
+	var menu: Control = menus[ui_name]
+	if fade_in:
+		menu.modulate = Color.TRANSPARENT
+		color_rect.modulate = Color.TRANSPARENT
+		fade_in(menu, fade_duration)
+	menu.show()
+	color_rect.show()	
 	get_tree().paused = true
 	
 	
-func close_ingame_ui(_args) -> void:
-	get_tree().paused = false
-	menus[open_ui_key].visible = false
+func fade_in(menu: Control, fade_duration: float = 1.0) -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(menu, "modulate", Color.WHITE, 1.0)
+	tween.parallel().tween_property(color_rect, "modulate", Color.WHITE, fade_duration)
+	
+	
+func fade_out(menu: Control, fade_duration: float = 1.0) -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(menu, "modulate", Color.TRANSPARENT, 1.0)
+	tween.parallel().tween_property(color_rect, "modulate", Color.TRANSPARENT, fade_duration)
+	tween.tween_callback(color_rect.hide)
+	tween.tween_callback(menu.hide)
+	
+	
+func close_ingame_ui(args: Array = []) -> void:
+	var fade_out: bool = false
+	var fade_duration: float = 1.0
+	
+	match len(args):
+		3:
+			fade_out = args[2]
+		4:
+			fade_out = args[2]
+			fade_duration = args[3]
+	
+	var menu: Control = menus[open_ui_key]
+	get_tree().paused = false	
+	if fade_out:
+		fade_out(menu, fade_duration)
+		return
+	menu.hide()
 	color_rect.hide()
 	
 	
